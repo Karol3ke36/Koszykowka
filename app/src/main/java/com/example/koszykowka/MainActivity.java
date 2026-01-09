8367b5c4c06164c1a8b3afb58095881bec40c7c5
Karol3ke36 <Karolpucyk9@gmail.com>
2026-01-09 09:29:56 +0100

    live data - obserwowane dane

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.koszykowka.databinding.ActivityMainBinding;
@@ -31,7 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
         punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
 
-        binding.textViewPunkty.setText(punktyViewModel.getPunkty()+"");
+        punktyViewModel.getPunkty().observe(
+                this, new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer integer) {
+                        binding.textViewPunkty.setText(integer+"");
+                    }
+                }
+        );
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -45,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         punktyViewModel.addPunkty(1);
-                        binding.textViewPunkty.setText(String.valueOf(punktyViewModel.getPunkty()));
                     }
                 }
         );
@@ -54,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         punktyViewModel.addPunkty(2);
-                        binding.textViewPunkty.setText(String.valueOf(punktyViewModel.getPunkty()));
                     }
                 }
         );
@@ -63,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         punktyViewModel.addPunkty(3);
-                        binding.textViewPunkty.setText(String.valueOf(punktyViewModel.getPunkty()));
                     }
                 }
         );
